dac3ec9519dfb4f60361e31d83d0255144561b1a
chingyat <tsingyat@outlook.com>
2020-10-13 00:16:18 +0800

    C api

---
+++ src/bin/calc-repl.rs
@@ -0,0 +1,38 @@
+use std::io::{self, Write};
+use calc::parse_and_eval_expr;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    ctrlc::set_handler(move || {
+        println!("received Ctrl+C!");
+        std::process::exit(0);
+    })
+        .expect("Error setting Ctrl-C handler");
+
+
+    let mut expr = String::new();
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let mut counter = 0;
+    loop {
+        print!("IN  [{}]: ", counter);
+        stdout.flush()?;
+        stdin.read_line(&mut expr)?;
+        if expr.trim().len() == 0 {
+            continue;
+        }
+
+        let result = parse_and_eval_expr(expr.as_str());
+        match result {
+            Ok(n) => {
+                println!("OUT [{}]: {:?}", counter, n);
+            }
+
+            Err(e) => {
+                eprintln!("OUT [{}]: {:?}", counter, e);
+            }
+        }
+        expr.clear();
+        counter += 1;
+    }
+}
+
